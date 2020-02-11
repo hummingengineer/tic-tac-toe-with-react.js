a865a643d5c376b5d51b91b9ac78c0500454f988
hummingengineer <hummingengineer@gmail.com>
2020-02-11 22:39:55 +0900

    Making an Interactive Component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,20 @@ import './index.css';
 
 // Square 컴포넌트는 <button>을 렌더링
 class Square extends React.Component {
+  // 무언가를 "기억하기"위해 component는 state를 사용. 클래스에 생성자를 추가하여 state를 초기화. this.state는 정의된 React 컴포넌트에 대해 비공개로 간주해야 한다.
+  // JavaScript 클래스에서 하위 클래스의 생성자를 정의할 때 항상 super를 호출해야합니다. 모든 React 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성해야 한다.
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {this.props.value}  {/* 부모 Board 컴포넌트에서 자식 Square 컴포넌트로 prop을 전달 */}
+    // Square의 render 함수 내부에서 onClick 핸들러를 통해 this.setState를 호출하는 것으로 React에게 <button>을 클릭할 때 Square가 다시 렌더링해야 한다고 알리게 된다.
+    // 컴포넌트에서 setState를 호출하면 React는 자동으로 컴포넌트 내부의 자식 컴포넌트 역시 업데이트 한다.
+    <button className="square" onClick={() => this.setState({value: 'X'})}>  {/* onClick prop으로 함수를 전달. Square 컴포넌트를 클릭하면 "X"가 체크되도록 만듬. */}
+        {this.state.value}  {/* 부모 Board 컴포넌트에서 자식 Square 컴포넌트로 prop을 전달 */}
       </button>
     );
   }
