5f9e9be6def419af1d5a9ab3d709dba199ae2b7a
hummingengineer <hummingengineer@gmail.com>
2020-02-11 22:31:33 +0900

    Passing Data Through Props

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}  {/* 부모 Board 컴포넌트에서 자식 Square 컴포넌트로 prop을 전달 */}
       </button>
     );
   }
@@ -16,7 +16,7 @@ class Square extends React.Component {
 // Board 컴포넌트는 사각형 9개를 렌더링
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;  // Board 컴포넌트에서 Square 컴포넌트로 value prop을 전달.
   }
 
   render() {
